82210cba7acc2bbe504d6a4ee4054972f85c635c
Mohamednashaat932 <126196870+Mohamednashaat932@users.noreply.github.com>
2025-12-27 17:20:40 +0200

    Delete Graduation-Project-master directory

--- Graduation-Project-master/src/components/Input.tsx
+++
@@ -1,9 +0,0 @@
-export default function Input({ type, id }: { type: string; id: string }) {
-  return (
-    <input
-      type={type}
-      id={id}
-      className="rounded-xl border border-[#050715] bg-[#EAEDFA] shadow-[4px_4px_4px_4px_rgba(0,0,0,0.25)] py-2 px-4 w-full h-14 "
-    />
-  );
-}
